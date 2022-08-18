d21ff455b7cb7484ac8b309d1bd2459c8f89a748
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-08-18 12:08:23 -0400

    added auxillary pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'authenticate',
+    loadChildren: () => import('./authenticate/authenticate.module').then( m => m.AuthenticatePageModule)
+  },
 ];
 
 @NgModule({
