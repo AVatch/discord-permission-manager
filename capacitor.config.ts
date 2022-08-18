f6ed5d09b229d34625c81917f74e6467b45ab289
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-08-18 11:27:47 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'discord-permission-manager',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
