27dfd693484ba6b411fbb2b1b4aa263174f3ee82
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-08-25 10:48:41 -0400

    made runnable

--- bot/src/commands-delete.js
+++ bot/src/commands-delete.js
@@ -14,23 +14,26 @@ initializeApp({
 const clientId = process.env.DISCORD_CLIENT_ID;
 const token = process.env.DISCORD_BOT_TOKEN;
 
-const guildIds = (
-  await admin.firestore().collection("/servers").get()
-).docs.map((doc) => doc.id);
+const main = async () => {
+  const guildIds = (
+    await admin.firestore().collection("/servers").get()
+  ).docs.map((doc) => doc.id);
 
-const rest = new REST({ version: "10" }).setToken(token);
+  const rest = new REST({ version: "10" }).setToken(token);
 
-// for guild-based commands
+  // for guild-based commands
 
-await Promise.all(
-  guildIds.map(async (guildId) => {
-    try {
-      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-        body: [],
-      });
-      console.log(`Successfully deleted all guild commands for ${guildId}.`);
-    } catch (err) {
-      console.error(err);
-    }
-  })
-);
+  await Promise.all(
+    guildIds.map(async (guildId) => {
+      try {
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+          body: [],
+        });
+        console.log(`Successfully deleted all guild commands for ${guildId}.`);
+      } catch (err) {
+        console.error(err);
+      }
+    })
+  );
+};
+main();
--- bot/src/commands-register.js
+++ bot/src/commands-register.js
@@ -17,35 +17,38 @@ initializeApp({
 const clientId = process.env.DISCORD_CLIENT_ID;
 const token = process.env.DISCORD_BOT_TOKEN;
 
-const guildIds = (
-  await admin.firestore().collection("/servers").get()
-).docs.map((doc) => doc.id);
+const main = async () => {
+  const guildIds = (
+    await admin.firestore().collection("/servers").get()
+  ).docs.map((doc) => doc.id);
 
-const commands = [];
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+  const commands = [];
+  const commandsPath = path.join(__dirname, "commands");
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  commands.push(command.data.toJSON());
-}
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
 
-const rest = new REST({ version: "10" }).setToken(token);
+  const rest = new REST({ version: "10" }).setToken(token);
 
-await Promise.all(
-  guildIds.map(async (guildId) => {
-    try {
-      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-        body: commands,
-      });
-      console.log(
-        `Successfully registered application commands for ${guildId}.`
-      );
-    } catch (err) {
-      console.error(err);
-    }
-  })
-);
+  await Promise.all(
+    guildIds.map(async (guildId) => {
+      try {
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+          body: commands,
+        });
+        console.log(
+          `Successfully registered application commands for ${guildId}.`
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    })
+  );
+};
+main();
